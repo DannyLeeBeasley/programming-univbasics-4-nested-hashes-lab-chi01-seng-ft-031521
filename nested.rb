ab8558287c2a57ea70981347bed7fa3ae33ce64c
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-05 18:46:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,13 +57,6 @@ programmer_hash[:dennis_richie][:languages]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -79,14 +72,17 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash << :yukihiro_matsumoto
+    
+programmer_hash << :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      }
+      
+programmer_hash
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
-
-	programmer_hash = 
+  programmer_hash = 
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -102,12 +98,10 @@ def changing_alan
       }
     }
 
+programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -123,5 +117,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] << "Assembly"
 end
